4868d8e4735838886df7732e5cf1145bfe3ec0b4
Pietersielie <pietersieliepcp@gmail.com>
2023-03-05 15:30:44 +0200

    Version 2.5.0, moving things over to Github as an excuse to learn how to use Github Actions.

---
+++ data-final-fixes.lua
@@ -0,0 +1,2 @@
+require("prototypes/entities/buildings/advanced-chemical-plant")
+require("prototypes/entities/buildings/advanced-assembling-machine")
---
+++ settings.lua
@@ -0,0 +1,83 @@
+data:extend({
+    {
+        type = "bool-setting",
+        name = "K2RFAC-bool-add-fuel-recipes-to-chem-plant-setting",
+        setting_type = "startup",
+        default_value = true
+    }
+})
+
+data:extend({
+    {
+        type = "bool-setting",
+        name = "K2RFAC-bool-add-atmo-condensation-recipes-to-chem-plant-setting",
+        setting_type = "startup",
+        default_value = true
+    }
+})
+
+data:extend({
+    {
+        type = "bool-setting",
+        name = "K2RFAC-bool-add-electrolysis-recipes-to-chem-plant-setting",
+        setting_type = "startup",
+        default_value = true
+    }
+})
+
+data:extend({
+    {
+        type = "bool-setting",
+        name = "K2RFAC-bool-add-filtration-recipes-to-chem-plant-setting",
+        setting_type = "startup",
+        default_value = true
+    }
+})
+
+data:extend({
+    {
+        type = "bool-setting",
+        name = "K2RFAC-bool-use-added-crusher-recipes-setting",
+        setting_type = "startup",
+        default_value = true
+    }
+})
+
+data:extend({
+	{
+		type = "bool-setting",
+		name = "K2RFAC-bool-add-SE-washing-recipes-to-chem-plant-setting",
+		setting_type = "startup",
+		default_value = false,
+		hidden = true
+	}
+})
+
+data:extend({
+	{
+		type = "bool-setting",
+		name = "K2RFAC-bool-add-SE-pulverising-recipes-to-assembler-setting",
+		setting_type = "startup",
+		default_value = false,
+		hidden = true
+	}
+})
+
+data:extend({
+	{
+		type = "bool-setting",
+		name = "K2RFAC-bool-add-SE-core-fragment-recipes-to-assembler-setting",
+		setting_type = "startup",
+		default_value = false,
+		hidden = true
+	}
+})
+
+local s1 = data.raw["bool-setting"]["K2RFAC-bool-add-SE-washing-recipes-to-chem-plant-setting"]
+local s2 = data.raw["bool-setting"]["K2RFAC-bool-add-SE-pulverising-recipes-to-assembler-setting"]
+local s3 = data.raw["bool-setting"]["K2RFAC-bool-add-SE-core-fragment-recipes-to-assembler-setting"]
+if mods['space-exploration'] then
+	s1.hidden = false
+	s2.hidden = false
+	s3.hidden = false
+end
