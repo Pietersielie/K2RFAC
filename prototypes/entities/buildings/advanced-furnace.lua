ac6b4ed8e730839cbbe2c2bfaa9b03197a965daf
Cilliers Pretorius <41905461+Pietersielie@users.noreply.github.com>
2024-08-27 15:08:52 +0200

    Update advanced-furnace.lua
    Fix a crash that occurs when only K2 is loaded, not SE as well.

--- prototypes/entities/buildings/advanced-furnace.lua
+++ prototypes/entities/buildings/advanced-furnace.lua
@@ -1,5 +1,7 @@
 local K2RFACList = data.raw["assembling-machine"]["kr-advanced-furnace"].crafting_categories
 
-if (settings.startup["K2RFAC-bool-add-SE-casting-recipes-to-furnace-setting"].value) then
-	table.insert(K2RFACList, "casting")
+if (mods['space-exploration']) then
+	if (settings.startup["K2RFAC-bool-add-SE-casting-recipes-to-furnace-setting"].value) then
+		table.insert(K2RFACList, "casting")
+	end
 end
